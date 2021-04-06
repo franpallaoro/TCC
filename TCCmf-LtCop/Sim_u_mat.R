fe65238c26c56c867b259e605ea2c572db6a767a
Rodrigo <rodrigo.moni@inf.ufrgs.br>
2021-04-06 04:51:11 -0300

    simulação

---
+++ TCCmf-LtCop/Sim_u_mat.R
@@ -0,0 +1,250 @@
+Sim_x_mat = function(N, G, T, params, n_vec, ind_t_dist, ind_Rt, f_hat_vec){
+#entrar com A B omega e nu
+#entrar com x inicial sefor 0 não precisa
+#entrar com indicadora se é t ou gaussian
+#entrar com n_vec que é o número de ativos por grupo
+#entrar com T, G e N
+#sair com PITs
+
+g_vec_cum            = cumsum(seq(G, 1, by = -1))
+k                    = G
+p                    = G*(G+1)/2
+
+A_vec     = params[1]
+B_vec     = params[2]
+end       = length(params)
+nu        = params[end]
+omega_vec = (1-B_vec)%*%t(f_hat_vec)
+
+f_mat               = matrix(0, nrow = T, ncol = p)
+s_mat               = matrix(0, nrow = T, ncol = p)
+x_mat               = matrix(0, nrow = T, ncol = N)
+
+if(ind_Rt==1){
+  R_mat   = array(0, dim = c(G, G, T)) 
+}else{
+  R_mat   = vector() 
+}
+
+
+f_mat[1,] = f_hat_vec
+
+
+teller_0 = 1
+asset_group_vec_new = matrix(0, nrow = N, ncol = 1)
+for(m in 1:G){
+  asset_group_vec_new[teller_0:(teller_0+n_vec[m]-1)] = m
+  teller_0                                          = teller_0 + n_vec[m]
+}
+
+
+S_l_mat    = Compute_S_l_matrix(asset_group_vec_new)[[1]] 
+
+Ind_lambda_to_group_vec = vector()
+for(i in 1:G){
+  Ind_lambda_to_group_vec = c(Ind_lambda_to_group_vec, t(i:G))
+}
+Ind_lambda_to_group_mat      = cbind(1:p, Ind_lambda_to_group_vec)
+
+# we are now ready to build "dau_L_tilde_dau_lambda_tilde"
+dau_L_tilde_dau_lambda_tilde = matrix(0, nrow = N*k, ncol = p)
+
+ind_match_lambda_group_cell = matrix(list(), G, 1) 
+
+
+for(i in 1:G){
+  # step 2
+  if(i == 1){
+    aux = seq(from = 1, to = sum(n_vec[1:i])*G, by = G)
+    dau_L_tilde_dau_lambda_tilde[aux,i] = 1 
+  }else{
+    aux = seq(from = sum(n_vec[1:(i-1)])*G+1, to = sum(n_vec[1:i])*G, by = G)
+    dau_L_tilde_dau_lambda_tilde[aux,i] = 1 
+  }
+  
+  
+  # step 3
+  ind_lambda_g_group_i           = Ind_lambda_to_group_mat[Ind_lambda_to_group_mat[ ,2] ==i, 1]
+  ind_match_lambda_group_cell[[i]] = ind_lambda_g_group_i
+  
+  # step 4
+  for(j in 1:(i-1)){
+    lambda_j                   = ind_lambda_g_group_i[j+1]
+    aux1 = seq(sum(n_vec[1:i-1])*G+1+j, sum(n_vec[1:i])*G+j, by = G)
+    dau_L_tilde_dau_lambda_tilde[aux1,lambda_j] =1
+  }
+}
+
+for(j in 1:T){
+  # Step 0: compute the time varying coefficient and the
+  #         data vector at this time
+  
+  f_t_vec   = f_mat[j,]
+  f_t_vec_2 = f_t_vec^2
+  x_j       = t(x_mat[j,])
+  
+  # Build the matrix L_til_prime_mat_t. See (F.19) of the web
+  # appendix.                 
+  # We also build a second matrix L_tilde_prime_mat_tg (F.19)
+  # with N_g = 1 for all values of g)                 
+  # This G x G matrix is based on only unique elements of f_j,t.
+  # We (could) use this later on to build the G x G block correlation
+  # matrix with within and between group correlations.
+  
+  teller_i        = 1
+  denom_f_un      = matrix(1, nrow = G, ncol = 1)
+  denom_f_vec     = matrix(1, nrow = N, ncol = 1)
+  f_prime_mat_t   = matrix(0, nrow = N, ncol = G)
+  f_prime_mat_tg  = matrix(0, nrow = G, ncol = G)
+  
+  for(i in 1:k){ # erro: i = 10, último dimensao da sl_ma
+    if(i != G){
+      g_i_c           = g_vec_cum[i]
+      denom_f_vec     = denom_f_vec + S_l_mat[,i:G]%*%f_t_vec_2[teller_i:g_i_c]  # Based on (F.19) and used repeatedly. 
+      denom_f_un      = denom_f_un + c(matrix(0, nrow = i-1, ncol = 1), f_t_vec_2[teller_i:g_i_c])  # This is (F.21) and used repeatedly.
+      f_prime_mat_t[,i]  = S_l_mat[,i:G]%*%f_t_vec[teller_i:g_i_c]
+      f_prime_mat_tg[,i] = c(matrix(0, nrow = i-1, ncol = 1), f_t_vec[teller_i:g_i_c])
+      teller_i            = g_i_c+1
+    }else{
+      g_i_c           = g_vec_cum[i]
+      denom_f_vec     = denom_f_vec + S_l_mat[,i:G]*f_t_vec_2[teller_i:g_i_c]  # Based on (F.19) and used repeatedly. 
+      denom_f_un      = denom_f_un + c(matrix(0, nrow = i-1, ncol = 1), f_t_vec_2[teller_i:g_i_c])  # This is (F.21) and used repeatedly.
+      f_prime_mat_t[,i]  = S_l_mat[,i:G]*f_t_vec[teller_i:g_i_c]
+      f_prime_mat_tg[,i] = c(matrix(0, nrow = i-1, ncol = 1), f_t_vec[teller_i:g_i_c])
+      teller_i            = g_i_c+1
+    }
+    
+  }
+  
+  # Step 1b: compute the correlation matrix (if ind_Rt=1) and its
+  #          inverse and determinant
+  denom_f_mat             = denom_f_vec%*%matrix(1, nrow = 1,ncol = k)
+  lambda_til_prime_mat_t  = f_prime_mat_t/sqrt(denom_f_mat)
+  sigma_2_vec_t           = (1/denom_f_vec)
+  aux_Compute_R_t_inv_and_det_R_t_L_matrix = Compute_R_t_inv_and_det_R_t_L_matrix(sigma_2_vec_t,lambda_til_prime_mat_t)
+  R_inv_t                 = as.matrix(aux_Compute_R_t_inv_and_det_R_t_L_matrix[[1]])
+  det_R_t                 = aux_Compute_R_t_inv_and_det_R_t_L_matrix[[2]]
+  Flag                    = aux_Compute_R_t_inv_and_det_R_t_L_matrix[[3]]
+  if(ind_Rt==1){
+    denom_f_mat_tg          = denom_f_un%*%matrix(1,1,G)
+    L_tilde_prime_mat_tg    = f_prime_mat_tg/sqrt(denom_f_mat_tg)
+    Rt_Block                = L_tilde_prime_mat_tg%*%t(L_tilde_prime_mat_tg)
+    R_mat[,,j] = Rt_Block
+  }
+  
+  if(Flag==1 || is.nan(det_R_t)){
+    loglike_vec = 1e14
+    break 
+  }else{
+    
+    #Step 2: Compute the likelihood for time t=j and store it,
+    #         compute the score function and update the time-varying
+    #         parameters
+    
+    # Compute the score is factor-model-specific.
+    
+    # Step 2a: compute R^{-1} x(t) and x(t)' R^{-1} x(t)                    
+    coef_R_1    = -0.5
+    R_inv_x     = R_inv_t %*% t(x_j)
+    x_R_inv_x   = x_j%*%R_inv_x
+    # Step 2b: compute the derivative of the copula density with respect to
+    #          R(t) as in (A8)-(A9)                    
+    if(ind_t_dist==1){
+      coef_R_2 = 0.5*(nu + N)/(nu-2+ x_R_inv_x ) 
+    }else{
+      coef_R_2 = 0.5 
+    }
+    R_2 = R_inv_x%*% t(R_inv_x)  # outer product: R^{-1} x(t) x(t)' R^{-1}
+    
+    # dau_log_c_dau_vec_R
+    A_mat = as.numeric(coef_R_2) * R_2 + coef_R_1 * R_inv_t 
+    
+    # Step 2c: compute the score, using  (F.2),(F.3) (F.20)-(F.25)
+    # of the online appendix.
+    
+    # auxilary term used repeatedly 
+    denom_f_un_3_2           = denom_f_un^(3/2)
+    
+    # (F.22) for j = 1,2,..., G(G+1/2)
+    dau_lambda_tilde_dau_f_vec  = 1/sqrt(denom_f_un[Ind_lambda_to_group_mat[,2]]) - t(f_t_vec_2)/denom_f_un_3_2[Ind_lambda_to_group_mat[,2]]
+    dau_lambda_tilde_dau_f_mat  = diag(length(dau_lambda_tilde_dau_f_vec))
+    diag(dau_lambda_tilde_dau_f_mat) = dau_lambda_tilde_dau_f_vec             
+    
+    dau_sigma2_dau_f_mat         = matrix(0, G, p)
+    
+    # fill dau_lambda_tilde_dau_f and dau_sigma2_dau_f
+    
+    # The loop below computes (F.23) for all values of j, looping over all G columns. 
+    # That is: check which values of f_(t,j) are in column g. Then compute the cross deratives.                     
+    
+    # The loop also computes (F.25) for each g (g = 1,...,G) 
+    # That is: check which values of f_(t,j) are in column g. 
+    # Then compute (F.25) w.r.t. to all these f_(t,j) values.                      
+    for(g in 1:G){
+      # find out which f_(t,j) are in column g
+      ind_cross_lambda_g =  ind_match_lambda_group_cell[[g]]
+      
+      if(g>1){
+        for(m in 1:g){
+          ind_2   = ind_cross_lambda_g
+          ind_m1  = ind_cross_lambda_g[m]
+          ind_2 = ind_2[-m]
+          
+          #(F.23)
+          dau_lambda_tilde_i_dau_m_vec             = -(f_t_vec[ind_m1]*f_t_vec[ind_2])/denom_f_un_3_2[g]
+          dau_lambda_tilde_dau_f_mat[ind_m1,ind_2] = dau_lambda_tilde_i_dau_m_vec
+        }
+      }
+      
+      #(F.25)
+      dau_sigma2_dau_f_mat[g,ind_cross_lambda_g] = -2*f_t_vec[ind_cross_lambda_g]/(denom_f_un[g]^2)
+    }
+    
+    
+    #(F.20)
+    dau_L_tilde_dau_f_mat =  dau_L_tilde_dau_lambda_tilde%*%dau_lambda_tilde_dau_f_mat
+    
+    
+    # (F.24)                                         
+    dau_D_dau_f_mat = S_l_mat%*%dau_sigma2_dau_f_mat
+    
+    # combining above with (F.2) and (F.3)
+    L_A         = t(lambda_til_prime_mat_t)%*%A_mat
+    s_vec_a     = 2* t(c(L_A))%*%dau_L_tilde_dau_f_mat
+    s_vec_b     = t(diag(A_mat))%*%dau_D_dau_f_mat
+    s_mat[j,]  = s_vec_a + s_vec_b
+    
+    # Step 2d: update the time-varying parameters using the score for the next
+    #          iteration
+    if(j<T){
+      f_mat[j+1,] = omega_vec +  A_vec*s_mat[j,] + B_vec*f_mat[j,] 
+      #adicionar aqui o x_mat[j+1,] = b.1 apendice
+      lambda_tilde_x_mat = f_mat[j+1,]/sqrt(1+t(f_mat[j+1,])%*%f_mat[j+1,])
+      sigma_x_mat = sqrt(1/(1+t(f_mat[j+1,])%*%f_mat[j+1,]))
+      if(!require(invgamma)){install.packages("invgamma")}
+      if(!require(mvtnorm)){install.packages("mvtnorm")}
+      library(invgamma)
+      library(mvtnorm)
+      gi = rinvgamma(1, 1/2*nu, 1/2*nu)
+      zt = rmvnorm(1, sigma = diag(p))
+      eps = rnorm(1)
+      x_mat[j+1,] = sqrt(gi)*(lambda_tilde_x_mat%*%t(zt) + sigma_x_mat*eps)
+    }
+   
+    
+    }
+  }
+return(x_mat)
+
+}
+  
+  #como transformar x_mat em PIT, inversa???
+  
+  # if (ind_t_dist == 1){
+  #   end = length(params)
+  #   nu      = params[end] #parametro da inversa gamma
+  #   x_mat_0 = qt(u_mat, nu) 
+  #   x_mat   = sqrt((nu-2)/nu) * x_mat_0
+  # }else{
+  #   x_mat   = qnorm(u_mat)
+  # }
--- TCCmf-LtCop/main.R
+++ TCCmf-LtCop/main.R
@@ -10,14 +10,11 @@ N          = dim(u_mat)[2]
 ind_se     = 1
 
 # information about group size and group allocation
-n_vec           = 10*matrix(1,10,1)            # number of assets per group
+n_vec           = matrix(10,10,1)            # number of assets per group
 asset_group_vec = kronecker(t(1:10),matrix(1,10,1))
 G               = max(asset_group_vec)     #number of groups
 
 
-##########################################################################
-##########################################################################
-############### Matlab routine to set up constrained optimization ########
 
 #install.packages("pracma")
 #install.packages("NlcOptim")
@@ -25,27 +22,13 @@ library(pracma)
 library(NlcOptim)
 
 
-#options = optimset('fmincon');
-#options = optimset(options, 'Display', 'off');
-#options = optimset(options, 'Diagnostics', 'off');
-#options = optimset(options, 'Algorithm', 'sqp');
 MaxIter     = 30000
 MaxFunEvals = 30000
 
 # Create starting values and lower/upper bounds for the contrained optimization
-
-#para_start_1f_eq  = [0.001 0.01 0.92 30]';
-#para_start_1f_gr  = [0.001*ones(1,G) 0.02 0.9 30]';
-#para_start_2f = [0.001*ones(1,G) 0.02 0.01*ones(1,2) 0.9 30]';
 para_start_Mf = c(0.05, 0.02*matrix(1,1,G), 0.01*matrix(1,1,2), 0.9, 30)
 
 # lower and upper bounds
-#Lb_1f_eq = [-1 eps*ones(1,2) 2.1]';
-#Rb_1f_eq = [ 0.1 0.10 0.99999 5000]';
-#Lb_1f_gr = [-1*ones(1,G) eps*ones(1,2) 2.1]';
-#Rb_1f_gr = [0.1*ones(1,G) 0.10 0.99999 5000]';
-#Lb_2f = [-1*ones(1,G+1) eps*ones(1,3) 2.1]';
-#Rb_2f = [0.1*ones(1,G+1) 0.1 0.1 0.99999 5000]';
 Lb_Mf = c(.Machine$double.eps*matrix(1, 1, 4+10), 2.1) #[ eps*ones(1,4+G) 2.1]';
 Rb_Mf = c(0.5*matrix(1,1,G+1), 0.2, 0.2, 0.99999, 5000)
 
@@ -63,29 +46,6 @@ Lb_Mf_LT_2 = c(.Machine$double.eps*matrix(1,1,2), 2.1)
 Rb_Mf_LT_2 = c(5*matrix(1,1,1), 0.99999, 5000)
 para_start_Mf_LT_2 = c(0.01, 0.96, 20)
             
-#create a empty cell array for parameters, standard errors (optional) and strings (will be used later on to print the results)
-# para_cell = cell(10,1);
-# if ind_se==1
-# se_para_cell = cell(10,1);
-# end
-# para_cell_str = cell(10,1);
-# #para_1f_eq_str = {'omega', 'A','B', 'nu Cop'};
-# #para_1f_gr_str = {'omega_1','omega_2','omega_3','omega_4','omega_5','omega_6','omega_7','omega_8','omega_9','omega_10', 'A','B', 'nu Cop'};
-# #para_2f_str = {'omega_eq','omega_1','omega_2','omega_3','omega_4','omega_5','omega_6','omega_7','omega_8','omega_9','omega_10', 'A_eq','A_gr','B', 'nu Cop'};
-# para_Mf_str = para_2f_str;
-# para_Mf_LT_str = {'A','B', 'nu Cop'};
-#             
-# para_cell_str{1} = para_1f_eq_str(1:end-1);
-# para_cell_str{2} = para_1f_eq_str;
-# para_cell_str{3} = para_1f_gr_str(1:end-1);
-# para_cell_str{4} = para_1f_gr_str;
-# para_cell_str{5} = para_2f_str(1:end-1);
-# para_cell_str{6} = para_2f_str;
-# para_cell_str{7} = para_Mf_str(1:end-1);
-# para_cell_str{8} = para_Mf_str;
-# para_cell_str{9} = para_Mf_LT_str(1:end-1);
-# para_cell_str{10} = para_Mf_LT_str;
-#             
 
 # step 1: moment estimator of f_bar (see eq 11/12 of Opschoor et al. (2020))
 # NOTE: DATA IS ALREADY BEEN SORTED ACROSS INDUSTRY (from 1 to ..... 10)
@@ -119,4 +79,45 @@ step2 = fmincon(x0 = para_start_Mf_LT_2, fn = LogLik_Copula_LT_factor_given_omeg
 # used to print the results
 theta_opt = step2[[1]]
 max_LogLik = -step2[[2]]
-            
+
+
+#simulação 
+
+sim = list()
+nsim = 500
+for (i in 1:nsim) {
+ sim[[i]] = Sim_u_mat(N+1, G, T, theta_opt, n_vec, 1, 0, f_bar)
+ print(i)
+}
+u_mat_sim = Reduce(`+`, sim) / length(sim)
+u_mat_sim = u_mat_sim[-1,]          
+u_mat_sim = as.matrix(u_mat_sim)
+T          = dim(u_mat_sim)[1]
+N          = dim(u_mat_sim)[2]
+
+n_vec           = matrix(10,10,1)            
+asset_group_vec = kronecker(t(1:10),matrix(1,10,1))
+G               = max(asset_group_vec)    
+
+
+x_mat    = qnorm(u_mat_sim)#norminv(u_mat);
+Rt_Block = Compute_BLOCK_correlation_matrix(x_mat,n_vec)[[1]]
+Rt_sample_vech = Sym2Vech(G,t(Rt_Block))
+
+
+Moment_based_omega_LT_FC_Optim <- function(G, params, rho_vec_sample_bl_vech) {
+  Moment_based_omega_LT_FC(G = G, params = params, rho_vec_sample_bl_vech = Rt_sample_vech)[[1]]
+}
+step1_sim = fmincon(x0 = para_start_Mf_LT_1, fn = Moment_based_omega_LT_FC_Optim, 
+                lb = Lb_Mf_LT_1, ub = Rb_Mf_LT_1, maxfeval = MaxFunEvals, maxiter = MaxIter,
+                G = G, rho_vec_sample_bl_vech = Rt_sample_vech)
+f_bar_sim = step1_sim$par
+
+
+LogLik_Copula_LT_factor_given_omega_Optim <- function(N,T,params,f_hat_vec,u_mat,asset_group_vec,n_vec,ind_t_dist,ind_Rt){
+  LogLik_Copula_LT_factor_given_omega(N,T,params,f_bar_sim,u_mat_sim,asset_group_vec,n_vec,1,0)[[1]]
+}
+step2_sim = fmincon(x0 = para_start_Mf_LT_2, fn = LogLik_Copula_LT_factor_given_omega_Optim, 
+                lb = Lb_Mf_LT_2, ub = Rb_Mf_LT_2, maxfeval = MaxFunEvals, maxiter = MaxIter,
+                N = N, T = T, f_hat_vec = f_bar_sim, u_mat = u_mat_sim, asset_group_vec = asset_group_vec, 
+                n_vec = n_vec, ind_t_dist = 1, ind_Rt = 0)
